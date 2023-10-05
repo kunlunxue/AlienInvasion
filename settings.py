9becab9363df3268a670ab4536915b51c0b7285c
张明 <1501423845@qq.com>
2023-10-05 21:36:24 +0800

    initialize Alien

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    def __init__(self):
+        """A class to store all settings for Alien Invasion"""
+
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (100, 149, 237)
