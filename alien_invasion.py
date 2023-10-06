0297851c5736c731f6ce43b54e110f2a76d76551
frank <zhangaili1372@gmail.com>
2023-10-06 18:35:19 +0800

    Waiting for adding bullet function

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,6 +22,7 @@ class AlienInvasion:
         while True:
             # Watch keyboard and mouse events.
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -30,8 +31,24 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
+                self._check_keydown_event(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_event(event)
+
+    def _check_keydown_event(self, event):
+        """Response to keypress"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_event(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
